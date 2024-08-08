0f13e0c0d8b08408292d000877ecf44636b7e84e
whatqt <trutnev.07@list.ru>
2024-08-09 03:31:00 +0700

    deleted:    register/editing_password.py 	modified:   register/views.py 	deleted:    review_films/test.py

--- register/views.py
+++ register/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
-from .editing_password import password_to_hash
-from .models import Test_accounts
-from django.db.utils import IntegrityError
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
@@ -12,23 +9,20 @@ from django.contrib.auth.models import User
 def index_home(request: HttpRequest):
     return HttpResponse('Главная страница')
 
-
 def log_in(request: HttpRequest):
     if request.method == 'POST':
         try:
             user_name_post = request.POST.get('username').replace(' ', '')
             password_post = request.POST.get('password').replace(' ', '')
             print(user_name_post)
-            print(password_post)
-            data_user = User.objects.filter(username=user_name_post).values()[0]
-            print(User.objects.filter(username=user_name_post))
-            print(data_user["is_staff"])            
+            print(password_post)          
             user = authenticate(request, username=user_name_post, password=password_post)
             print(user)
             login(request, user)
-            if data_user["is_staff"] is True:
+            if request.user.is_staff is True:
                 return redirect('/admin_panel/')
-            return redirect('/select_films/')
+            else:
+                return redirect('/select_films/')
         except (IndexError, AttributeError):
             return HttpResponse(f'<h1>Неверный логин или пароль!</h1>')
     else:
@@ -41,12 +35,12 @@ def register(request: HttpRequest):
         check_repeat_name = User.objects.filter(username=user_name_post)
         print(check_repeat_name)
         if check_repeat_name:
-            return HttpResponse('Такой ник уже занят')            
+            return HttpResponse('<h1>Такой ник уже занят</h1>')            
         User.objects.create_user(user_name_post, None, password_post)
         return redirect('/register/log_in')
     else:
         if request.user.is_anonymous is False:
-            return HttpResponse('Нет доступа', status=400)
+            return HttpResponse('<h1>Нет доступа</h1>', status=400)
         else:
             print(request.user)
             print(request.user.is_anonymous)            
@@ -54,11 +48,11 @@ def register(request: HttpRequest):
 
 @login_required(login_url='/register/log_in')
 def films(request: HttpRequest):
-    return HttpResponse('Здесь будут фильмы')
+    return HttpResponse('<h1>Здесь будут фильмы</h1>')
 
 @login_required(login_url='/register/log_in')
 def log_out(request: HttpRequest):
     logout(request)
-    return HttpResponse('Вы вышли из системы')
+    return HttpResponse('<h1>Вы вышли из системы</h1>')
 
 
